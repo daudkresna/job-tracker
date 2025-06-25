c8439829f9e3586889c82bd30e357a4e21ea2aab
Daud Kresna Dwiva <daudkresna@gmail.com>
2025-06-25 20:55:29 +0700

    add sign in and sign out button

---
+++ components/auth-button.tsx
@@ -0,0 +1,39 @@
+import { auth, signIn, signOut } from "@/auth";
+
+const LoginButton = () => {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("github", { redirectTo: "/" });
+      }}
+    >
+      <button type="submit">Sign In with GitHub</button>
+    </form>
+  );
+};
+
+const LogoutButton = () => {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <button type="submit">Sign Out</button>
+    </form>
+  );
+};
+
+const AuthButton = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return <LoginButton />;
+  } else {
+    return <LogoutButton />;
+  }
+};
+
+export default AuthButton;
--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,21 @@
 import { Briefcase } from "lucide-react";
 import Cards from "./cards";
+import AuthButton from "./auth-button";
 
-export default function Header() {
+export default async function Header() {
   return (
     <>
-      <h1 className="flex items-center text-3xl gap-2 font-semibold">
-        <Briefcase className="h-8 w-8" /> Job Tracker
-      </h1>
-      <p className="text-gray-500 mb-8">
-        Manage and track your job applications in one place
-      </p>
+      <div className="flex flex-row justify-between items-center mb-8">
+        <div>
+          <h1 className="flex items-center text-3xl gap-2 font-semibold">
+            <Briefcase className="h-8 w-8" /> Job Tracker
+          </h1>
+          <p className="text-gray-500">
+            Manage and track your job applications in one place
+          </p>
+        </div>
+        <AuthButton />
+      </div>
       <Cards />
     </>
   );
